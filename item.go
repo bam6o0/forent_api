92db62d7ab7730fb327d4597d7f25f094fd3f8bd
takato sasagawa <takato.sasagawa@gmail.com>
2018-02-19 20:02:24 +0900

    fix package

--- item.go
+++ item.go
@@ -1,10 +1,14 @@
-package controller
+package main
 
 import (
 	"forent_api/app"
+	"forent_api/models"
 
 	"github.com/goadesign/goa"
 )
+
+// ErrDatabaseError is the error returned when a db query fails.
+var ErrDatabaseError = goa.NewErrorClass("db_error", 500)
 
 // ItemController implements the item resource.
 type ItemController struct {
@@ -18,12 +22,24 @@ func NewItemController(service *goa.Service) *ItemController {
 
 // Create runs the create action.
 func (c *ItemController) Create(ctx *app.CreateItemContext) error {
-	// ItemController_Create: start_implement
+	item := models.Item{}
+	item.Name = ctx.Payload.Name
+	item.Description = ctx.Payload.Description
+	item.Price = ctx.Payload.Price
+	item.Compensation = ctx.Payload.Compensation
+	item.UserID = ctx.Payload.UserID
+	item.CategoryID = ctx.Payload.CategoryID
+	item.PlaceID = ctx.Payload.PlaceID
+	item.Image1 = ctx.Payload.Image1
+	item.Image2 = *ctx.Payload.Image2
+	item.Image3 = *ctx.Payload.Image3
+	item.Image4 = *ctx.Payload.Image4
 
-	// Put your logic here
-
-	return nil
-	// ItemController_Create: end_implement
+	err := ItemDB.Add(ctx.Context, &item)
+	if err != nil {
+		return ErrDatabaseError(err)
+	}
+	return ctx.Created()
 }
 
 // Delete runs the delete action.
