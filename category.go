ca32670518391127d5bf01d1c6dd422787ebf044
takato sasagawa <takato.sasagawa@gmail.com>
2018-02-20 20:03:55 +0900

    fix category

--- category.go
+++ category.go
@@ -19,13 +19,6 @@ func NewCategoryController(service *goa.Service) *CategoryController {
 // List runs the list action.
 func (c *CategoryController) List(ctx *app.ListCategoryContext) error {
 	pay := *ctx.Payload
-	if pay.MiddlecategoryID != nil {
-		categories := CategoryDB.ListCategory(ctx.Context, *pay.MiddlecategoryID)
-		return ctx.OK(categories)
-	} else {
-		categories := CategoryDB.AllListCategory(ctx.Context)
-		return ctx.OK(categories)
-
-	}
-
+	categories := CategoryDB.ListCategory(ctx.Context, *pay.MiddlecategoryID)
+	return ctx.OK(categories)
 }
