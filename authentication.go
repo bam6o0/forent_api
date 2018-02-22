62ce15cf476df50bd1deb68690526da15e331935
takato sasagawa <takato.sasagawa@gmail.com>
2018-02-22 22:13:42 +0900

    fix sign

--- authentication.go
+++ authentication.go
@@ -61,13 +61,12 @@ func (c *AuthenticationController) Sigin(ctx *app.SiginAuthenticationContext) er
 	in10m := time.Now().Add(time.Duration(10) * time.Minute).Unix()
 	uuid, _ := uuid.NewV4()
 	token.Claims = jwtgo.MapClaims{
-		"iss":     "Issuer",          // who creates the token and signs it
-		"aud":     "Audience",        // to whom the token is intended to be sent
+		"iss":     "forent",          // who creates the token and signs it
+		"aud":     "forent-user",     // to whom the token is intended to be sent
 		"exp":     in10m,             // time when the token will expire (10 minutes from now)
 		"jti":     uuid.String(),     // a unique identifier for the token
 		"iat":     time.Now().Unix(), // when the token was issued/created (now)
 		"nbf":     2,                 // time before which the token is not yet valid (2 minutes ago)
-		"sub":     "subject",         // the subject/principal is whom the token is about
 		"scopes":  "api:access",      // token scope - not a standard claim
 		"user_id": user.ID,
 	}
@@ -116,13 +115,12 @@ func (c *AuthenticationController) Signup(ctx *app.SignupAuthenticationContext) 
 	in10m := time.Now().Add(time.Duration(10) * time.Minute).Unix()
 	uuid, _ := uuid.NewV4()
 	token.Claims = jwtgo.MapClaims{
-		"iss":     "Issuer",          // who creates the token and signs it
-		"aud":     "Audience",        // to whom the token is intended to be sent
+		"iss":     "forent",          // who creates the token and signs it
+		"aud":     "forent-user",     // to whom the token is intended to be sent
 		"exp":     in10m,             // time when the token will expire (10 minutes from now)
 		"jti":     uuid.String(),     // a unique identifier for the token
 		"iat":     time.Now().Unix(), // when the token was issued/created (now)
 		"nbf":     2,                 // time before which the token is not yet valid (2 minutes ago)
-		"sub":     "subject",         // the subject/principal is whom the token is about
 		"scopes":  "api:access",      // token scope - not a standard claim
 		"user_id": user.ID,
 	}
@@ -137,26 +135,6 @@ func (c *AuthenticationController) Signup(ctx *app.SignupAuthenticationContext) 
 	// Send response
 	return ctx.NoContent()
 
-}
-
-// GetUserByEmail gets a user by email
-func GetUserByEmail(db *gorm.DB, email string) (*app.User, error) {
-
-	userDb := models.User{}
-	// Get user id
-	c := db.Where("email = ?", email).First(&userDb)
-	var user app.User
-	user.ID = userDb.ID
-	user.Email = userDb.Email
-	user.Password = userDb.Password
-	user.Salt = userDb.Salt
-
-	exists := c.RecordNotFound()
-	if exists != true {
-		err := c.Error
-		return nil, err
-	}
-	return &user, nil
 }
 
 // CheckEmailExists is check email adress in user db
